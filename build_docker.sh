f68e6b5dcd0f76ab4c86621f4d833552f4f5333d
Sungtaek Lee <leesungtaek@gmail.com>
2017-03-16 17:41:28 +0900

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -5,7 +5,7 @@ echo RELEASE_VERSION=${RELEASE_VERSION}
 
 echo "# build azkaban-web-server image"
 cp build/distributions/azkaban-web-server-*.tar.gz docker/azkaban-webserver/dist/azkaban-web-server-${RELEASE_VERSION}.tar.gz
-docker build --build-arg AZK_VERSION=${RELEASE_VERSION} -t poporisil/azkaban3-webserver:${RELEASE_VERSION} docker/azkaban-webserver
+docker build --rm=false --build-arg AZK_VERSION=${RELEASE_VERSION} -t poporisil/azkaban3-webserver:${RELEASE_VERSION} docker/azkaban-webserver
 echo "# build azkaban-exec-server image"
 cp build/distributions/azkaban-exec-server-*.tar.gz docker/azkaban-execserver/dist/azkaban-exec-server-${RELEASE_VERSION}.tar.g
-docker build --build-arg AZK_VERSION=${RELEASE_VERSION} -t poporisil/azkaban3-execserver:${RELEASE_VERSION} docker/azkaban-execserver
+docker build --rm=false --build-arg AZK_VERSION=${RELEASE_VERSION} -t poporisil/azkaban3-execserver:${RELEASE_VERSION} docker/azkaban-execserver
