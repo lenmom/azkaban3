3a4e935ebd71c674eccbd8fd398f8f116fa5cd26
Sungtaek Lee <leesungtaek@gmail.com>
2017-03-16 22:03:05 +0900

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -3,9 +3,18 @@
 RELEASE_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo RELEASE_VERSION=${RELEASE_VERSION}
 
-echo "# build azkaban-web-server image"
-cp build/distributions/azkaban-web-server-*.tar.gz docker/azkaban-webserver/dist/azkaban-web-server-${RELEASE_VERSION}.tar.gz
-docker build --rm=false --build-arg AZK_VERSION=${RELEASE_VERSION} -t poporisil/azkaban3-webserver:${RELEASE_VERSION} docker/azkaban-webserver
-echo "# build azkaban-exec-server image"
-cp build/distributions/azkaban-exec-server-*.tar.gz docker/azkaban-execserver/dist/azkaban-exec-server-${RELEASE_VERSION}.tar.g
-docker build --rm=false --build-arg AZK_VERSION=${RELEASE_VERSION} -t poporisil/azkaban3-execserver:${RELEASE_VERSION} docker/azkaban-execserver
+echo "# build azkaban-web-server image..."
+PKG_FILE=$(ls build/distributions | grep azkaban-web-server)
+PKG_NAME=${PKG_FILE%.tar.gz}
+echo "# PKG_FILE=${PKG_FILE}"
+echo "# PKG_NAME=${PKG_NAME}"
+cp build/distributions/${PKG_FILE} docker/azkaban-webserver/dist/
+docker build --rm=false --build-arg PKG_FILE=${PKG_FILE} --build-arg PKG_NAME=${PKG_NAME} -t poporisil/azkaban3-webserver:${RELEASE_VERSION} docker/azkaban-webserver
+
+echo "# build azkaban-exec-server image..."
+PKG_FILE=$(ls build/distributions | grep azkaban-exec-server)
+PKG_NAME=${PKG_FILE%.tar.gz}
+echo "# PKG_FILE=${PKG_FILE}"
+echo "# PKG_NAME=${PKG_NAME}"
+cp build/distributions/${PKG_FILE} docker/azkaban-execserver/dist/
+docker build --rm=false --build-arg PKG_FILE=${PKG_FILE} --build-arg PKG_NAME=${PKG_NAME} -t poporisil/azkaban3-execserver:${RELEASE_VERSION} docker/azkaban-execserver
